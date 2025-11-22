85a91fe5aac88ea8855e5c2885c2a4c3de0ead8a
mahamudulPeas <mahamudulpeas@gmail.com>
2025-11-22 14:33:54 +0600

    HackerRank-String Intro Problem Solved

---
+++ src/hackerRank/LoopTwo.java
@@ -0,0 +1,21 @@
+package hackerRank;
+
+public class LoopTwo {
+    /*
+
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void seriesBuilder(){
+
+        int n = 10;
+        for (int i=0 ; i<n ; i++){
+            
+        }
+
+//        return null;
+    }
+}
